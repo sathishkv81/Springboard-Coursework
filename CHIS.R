5300c1380709994c087afba5245088ea4ec2d51f
sathishkv81 <sathishkumarv@gmail.com>
2016-07-02 16:35:44 +0530

    CHIS case study from DataCamp Tutorial

---
+++ CHIS.R
@@ -0,0 +1,64 @@
+# Load all packages
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(ggthemes)
+
+# Script generalized into a function
+mosaicGG <- function(data, X, FILL) {
+  
+  # Proportions in raw data
+  DF <- as.data.frame.matrix(table(data[[X]], data[[FILL]]))
+  DF$groupSum <- rowSums(DF)
+  DF$xmax <- cumsum(DF$groupSum)
+  DF$xmin <- DF$xmax - DF$groupSum
+  DF$X <- row.names(DF)
+  DF$groupSum <- NULL
+  DF_melted <- melt(DF, id = c("X", "xmin", "xmax"), variable.name = "FILL")
+  library(dplyr)
+  DF_melted <- DF_melted %>% 
+    group_by(X) %>% 
+    mutate(ymax = cumsum(value/sum(value)),
+           ymin = ymax - value/sum(value))
+  
+  # Chi-sq test
+  results <- chisq.test(table(data[[FILL]], data[[X]])) # fill and then x
+  resid <- melt(results$residuals)
+  names(resid) <- c("FILL", "X", "residual")
+
+  # Merge data
+  DF_all <- merge(DF_melted, resid)
+  
+  # Positions for labels
+  DF_all$xtext <- DF_all$xmin + (DF_all$xmax - DF_all$xmin)/2
+  index <- DF_all$xmax == max(DF_all$xmax)
+  DF_all$ytext <- DF_all$ymin[index] + (DF_all$ymax[index] - DF_all$ymin[index])/2
+  
+  # plot:
+  g <- ggplot(DF_all, aes(ymin = ymin,  ymax = ymax, xmin = xmin, 
+                          xmax = xmax, fill = residual)) + 
+  geom_rect(col = "white") +
+  geom_text(aes(x = xtext, label = X),
+            y = 1, size = 3, angle = 90, hjust = 1, show.legend = FALSE) +
+  geom_text(aes(x = max(xmax),  y = ytext, label = FILL),
+            size = 3, hjust = 1, show.legend = FALSE) +
+  scale_fill_gradient2("Residuals") +
+  scale_x_continuous("Individuals", expand = c(0,0)) +
+  scale_y_continuous("Proportion", expand = c(0,0)) +
+  theme_tufte() +
+  theme(legend.position = "bottom")
+  print(g)
+}
+
+# BMI described by age
+mosaicGG(adult, "SRAGE_P","RBMI")
+
+# Poverty described by age
+mosaicGG(adult,"SRAGE_P","POVLL")
+
+# mtcars: am described by cyl
+mosaicGG(mtcars,"cyl","am")
+
+# Vocab: vocabulary described by education
+library(car)
+mosaicGG(Vocab,"education","vocabulary")
